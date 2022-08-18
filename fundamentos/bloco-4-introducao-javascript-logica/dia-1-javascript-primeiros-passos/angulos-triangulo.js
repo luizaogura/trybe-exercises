778136291552108a1071ea36d2400b61ccffa8d1
Luiza Ogura <luizapogura@gmail.com>
2022-08-18 19:37:34 -0300

    Adicionando arquivos dos exercicios de 1 a 6

---
+++ fundamentos/bloco-4-introducao-javascript-logica/dia-1-javascript-primeiros-passos/angulos-triangulo.js
@@ -0,0 +1,13 @@
+const a = 100;
+const b = 35;
+const c = 45;
+
+console.log("Os ângulos formam um triangulo?", a, "e", b, "e", c);
+
+if( a + b + c <= 180){
+    console.log("True");
+}
+
+else{
+    console.log("False");
+}
---
+++ fundamentos/bloco-4-introducao-javascript-logica/dia-1-javascript-primeiros-passos/maior-de-2-numeros.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 35;
+
+console.log("Os valores são:", a, "e", b);
+
+if( a >= b){
+    console.log("O maior número é o:", a);
+}
+else{
+    console.log("O maior número é o:", b);
+}
