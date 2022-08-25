57479c4744555a44c5f572dbc61c5317da4fb716
Luiza Ogura <luizapogura@gmail.com>
2022-08-25 17:21:32 -0300

    Bloco 4

---
+++ fundamentos/bloco-4-introducao-javascript-logica/dia-4-javascript-objetos-e-funcoes/funcoes/exercicio1.js
@@ -0,0 +1,8 @@
+// Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
+
+let palavra = '';
+let contrario = '';
+
+for(let index = 0; index < palavra.length, index++){
+    contrario = contrario + 
+}
