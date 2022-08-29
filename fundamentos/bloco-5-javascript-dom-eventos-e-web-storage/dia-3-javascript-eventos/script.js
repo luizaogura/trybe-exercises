751e9d48d69cc269f31154b7fbb3f0359c4ba5e6
Luiza Ogura <luizapogura@gmail.com>
2022-08-29 19:41:39 -0300

    Adicionando exercicio 1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -0,0 +1,54 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  /* Exercício 1:
+Crie um calendário dinamicamente.
+O array decemberDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. 
+Sua função deve criar dinamicamente cada dia do calendário e os adicionar dentro da tag <ul>.
+A tag <ul> deve conter o id 'days';
+Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex: <li class="day">3</li>
+Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li>
+Os dias 4, 11, 18 e 25 são sextas-feiras. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li> */
+
+let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function createDaysOfTheMonth(){
+let getDaysList = document.querySelector("#days");
+
+    for(let index = 0; index < decemberDaysList.length; index += 1){
+    let day = decemberDaysList[index];
+    let dayItem = document.createElement("li");
+    dayItem.innerHTML = day;
+
+    if(day === 24 || day === 31) {
+        dayItem.className = "day holiday";
+        getDaysList.appendChild(dayItem);
+    }
+    else if(day === 4 || day === 11 | day === 18) {
+        dayItem.className = "day friday";
+        getDaysList.appendChild(dayItem);
+    }
+    else if(day === 25) {
+        dayItem.className = "day holiday friday";
+        getDaysList.appendChild(dayItem);
+    }
+    else {
+        dayItem.className = "day";
+        getDaysList.appendChild(dayItem);
+    }
+    };
+};
+
+createDaysOfTheMonth();
