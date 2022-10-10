def9ea3d57bbdceecfcbf05acec23eb6485c1fff
Luiza Ogura <luizapogura@gmail.com>
2022-10-10 18:29:03 -0300

    Dia 1.1

---
+++ front-end/bloco-1-javascript-e-testes-assincronos/dia-1.1/sendMarsTemperature.js
@@ -0,0 +1,27 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`Atualmente está ${toFahrenheit(temperature)}ºF em Marte`);
+
+const greet = (temperature) =>
+  console.log(`Olá! Curiosity aqui. Nesse momento está ${temperature}ºC em Marte`);
+
+// crie a função sendMarsTemperature abaixo
+
+const sendMarsTemperature = () => {
+    const currentTemperature = getMarsTemperature();
+    setTimeout(() => console.log(`A temperatura de Marte é: ${currentTemperature} graus celsius`), messageDelay());
+    const delay = messageDelay();
+    console.log(`Tempo de delay = ${delay}`)
+}
+
+sendMarsTemperature(temperatureInFahrenheit); // Imprime "Atualmente está 46.4ºF em Marte", por exemplo
+
+sendMarsTemperature(greet); // Imprime "Olá! Curiosity aqui. Nesse momento são 36ºC em Marte", por exemplo
